1e9ab8f179172c1f8961504e894747a8c58cbe8a
xiaozhang <1538583867@qq.com>
2026-03-18 21:53:38 +0800

    chore(examples): 新增用例配置

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,13 @@
+import { core } from '@webrtc-player/core';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello, world!{core.a}</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
